d30b020e4f34655a04b4baf49a382f20587e8a71
Florian Benkö <florian.benkoe2@web.de>
2017-07-23 09:32:49 +0200

    Update example.py

--- example.py
+++ example.py
@@ -3,30 +3,30 @@ import requests
 class ebayreport():
     """one example for the trafficreport"""
     def gettrafficreport(accesstoken, timespan, listoflistings=[]):
-    """get request for trafficreport, if listoflisting is not specified it will set the dimension to Day
-    -> aggregated numbers on account level
-    it takes a timespan as input in the following format: [20170501..20170601]
-    """
-    metrics_trafficreport = ('CLICK_THROUGH_RATE,LISTING_IMPRESSION_SEARCH_RESULTS_PAGE,'
-                             'LISTING_IMPRESSION_STORE,LISTING_IMPRESSION_TOTAL,'
-                             'LISTING_VIEWS_SOURCE_DIRECT,LISTING_VIEWS_SOURCE_OFF_EBAY,'
-                             'LISTING_VIEWS_SOURCE_OTHER_EBAY,LISTING_VIEWS_SOURCE_SEARCH_RESULTS_PAGE,'
-                             'LISTING_VIEWS_SOURCE_STORE,LISTING_VIEWS_TOTAL,'
-                             'SALES_CONVERSION_RATE,TRANSACTION')
-    if listoflistings == []:
-        dimension_trafficreport = 'DAY'
-    else:
-        dimension_trafficreport = 'Listing'
-        
-    url_trafficreport = 'https://api.ebay.com/sell/analytics/v1/traffic_report'
+        """get request for trafficreport, if listoflisting is not specified it will set the dimension to Day
+        -> aggregated numbers on account level
+        it takes a timespan as input in the following format: [20170501..20170601]
+        """
+        metrics_trafficreport = ('CLICK_THROUGH_RATE,LISTING_IMPRESSION_SEARCH_RESULTS_PAGE,'
+                                 'LISTING_IMPRESSION_STORE,LISTING_IMPRESSION_TOTAL,'
+                                 'LISTING_VIEWS_SOURCE_DIRECT,LISTING_VIEWS_SOURCE_OFF_EBAY,'
+                                 'LISTING_VIEWS_SOURCE_OTHER_EBAY,LISTING_VIEWS_SOURCE_SEARCH_RESULTS_PAGE,'
+                                 'LISTING_VIEWS_SOURCE_STORE,LISTING_VIEWS_TOTAL,'
+                                 'SALES_CONVERSION_RATE,TRANSACTION')
+        if listoflistings == []:
+            dimension_trafficreport = 'DAY'
+        else:
+            dimension_trafficreport = 'Listing'
 
-    querystring = {"filter":str("marketplace_ids:{EBAY_DE},date_range:"+str(timespan)),
-                   "dimension":dimension_trafficreport,
-                   'metric':metrics_trafficreport}
+        url_trafficreport = 'https://api.ebay.com/sell/analytics/v1/traffic_report'
 
-    headers = {
-        'authorization': str('Bearer ' + accesstoken)
-    }
-    requesttype = 'GET'
+        querystring = {"filter":str("marketplace_ids:{EBAY_DE},date_range:"+str(timespan)),
+                       "dimension":dimension_trafficreport,
+                       'metric':metrics_trafficreport}
 
-    response = requests.request("GET", url_trafficreport, headers=headers, params=querystring)
+        headers = {
+            'authorization': str('Bearer ' + accesstoken)
+        }
+        requesttype = 'GET'
+
+        response = requests.request("GET", url_trafficreport, headers=headers, params=querystring)
